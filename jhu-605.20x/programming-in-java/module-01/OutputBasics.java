43bc035843c047275f745dca2a8bcafbdbb61cd7
justinthelaw <justinwingchunglaw@gmail.com>
2022-05-31 15:50:21 -0400

    lectures A-I

---
+++ jhu-605.20x/programming-in-java/module-01/OutputBasics.java
@@ -0,0 +1,16 @@
+public class OutputBasics {
+  public static void main(String[] args) {
+    int x = 10;
+    int y = 20;
+
+    System.out.println("x is " + x + " and y is " + y);
+
+    System.out.print("x is greater than y: ");
+
+    System.out.println(x > y);
+
+    System.out.println("(WRONG) x + y = " + x + y);
+
+    System.out.println("(RIGHT) x + y = " + (x + y));
+  }
+}
