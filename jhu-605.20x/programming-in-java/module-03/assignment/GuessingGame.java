6c270b87384619c47af047a1b02a9c141af77bf0
justinthelaw <justinwingchunglaw@gmail.com>
2022-06-15 21:08:43 -0700

    module-03 complete

---
+++ jhu-605.20x/programming-in-java/module-03/assignment/GuessingGame.java
@@ -0,0 +1,95 @@
+/*
+*  GuessingGame performs the required task, using certain assumptions, as described in GuessingGame.md or the PDF in which this code block is contained.
+*  author: Justin Law
+*  class: EN.605.201.82.SU22
+*/
+
+import java.util.Scanner;
+
+// Class which recieves max number of guesses and max number and
+// initiaites a guess game with guidance towards the correct answer
+public class GuessingGame {
+   public static void main(String[] args) {
+
+      // game state initialization
+      int gameState = 1;
+
+      while (gameState == 1)
+      { // run game while gameState = 1, "on"
+
+         // max possible number
+         int maxNumber = 1;
+         // max number of guesses
+         int numberOfGuesses = 1;
+         // current guess attempts
+         int currentAttempts = 0;
+         // initialize guess variable
+         int guess = 1;
+
+         // add space before game initialization inputs
+         System.out.println();
+         // All scanners stay "open" until program ends
+         // Use a Scanner to get user-provided max number
+         Scanner inputNumber = new Scanner(System.in);
+         System.out.print("Maximum possible number: ");
+         maxNumber = inputNumber.nextInt();
+         // Use a Scanner to get user-provided max guesses
+         Scanner inputGuesses = new Scanner(System.in);
+         System.out.print("Maximum number of guesses: ");
+         numberOfGuesses = inputGuesses.nextInt();
+
+         // generate random number
+         int randomNumber = (int) (maxNumber * Math.random()) + 1;
+
+         while (guess != randomNumber && gameState == 1)
+         {
+            // add space before guess prompts loop
+            System.out.println();
+            Scanner inputGuess = new Scanner(System.in);
+            System.out.print("Guess a number between 1 and " + maxNumber + ": ");
+            guess = inputGuess.nextInt();
+            currentAttempts++;
+
+            if (guess == randomNumber)
+            { // if correct guess, ask player if they want to play again
+               System.out.println("\nYou guessed the correct number!");
+               Scanner choice = new Scanner(System.in);
+               System.out.print("Play again? Yes (1) or No (2): ");
+               int playerChoice = choice.nextInt();
+               if (playerChoice != 1)
+               { // don't change game state and continue game
+                  gameState = 0;
+                  System.out.println("\nThanks for playing! Goodbye :)");
+               }
+            }
+            else
+            { // check answer and print appropriate message
+               if (guess < randomNumber)
+               {
+                  System.out.println("Too low!");
+               }
+               if (guess > randomNumber)
+               {
+                  System.out.println("Too high!");
+               }
+               if (currentAttempts >= numberOfGuesses) {
+                  System.out.println("Sorry, you've reached the maximum number of guesses!");
+                  // set the game to complete using condition
+                  guess = randomNumber;
+                  // ask player if they want to play again
+                  Scanner choice = new Scanner(System.in);
+                  // plyaer must type 1 for yes and 2 for no
+                  System.out.print("Play again? Yes (1) or No (2): ");
+                  int playerChoice = choice.nextInt();
+                  if (playerChoice != 1)
+                  {
+                     System.out.println("\nThanks for playing! Goodbye :)");
+                  }
+               }
+            }
+         }
+
+      }
+
+   } // end main()
+} // end class GuessingGame
--- jhu-605.20x/programming-in-java/module-03/assignment/GuessingGame.md
+++ jhu-605.20x/programming-in-java/module-03/assignment/GuessingGame.md
@@ -1,42 +1,45 @@
 # Instructions
 
-Write a program that asks a user to guess a secret number between 1 and N, where N is a positive
-number that the user is prompted for. The program should also prompt the user for the maximum
-number of guesses they would like to make. Each time the player makes a guess, the program shall
-respond with “correct”, “too low”, or “too high”. The program should keep track of the number of
-guesses the user made to discover the secret number. The program should continue execution until the
-user has discovered the secret number or has exceeded the maximum number of guesses. The program
-shall also allow the user to play the game again until the user declines. The program should generate
-the magic number randomly, using the built-in Java method Math.random(). To generate a random
-number between 1 and n, the following formula may be used:
+Write a program that asks a user to guess a secret number between 1 and N, where N is a positive number that the user is prompted for. The program should also prompt the user for the maximum number of guesses they would like to make. Each time the player makes a guess, the program shall respond with “correct”, “too low”, or “too high”. The program should keep track of the number of guesses the user made to discover the secret number. The program should continue execution until the user has discovered the secret number or has exceeded the maximum number of guesses. The program shall also allow the user to play the game again until the user declines. The program should generate the magic number randomly, using the built-in Java method Math.random(). To generate a random number between 1 and n, the following formula may be used:
 
 `randomNumber = (int) (n * Math.random() ) + 1`
 
 # Thought Process
 
 #### Discussion
-Below is a diagram of the use case and expected behaviour of GuessingGame.java generated using PlantUML, the problem statement givens, and the assumptions listed below the diagram. See the [PlantUML](#PlantUML) section
+
+Below is a diagram of the use case and expected behavior of GuessingGame.java generated using PlantUML, the problem statement givens, and the assumptions listed below the diagram. See the [PlantUML](#PlantUML) section.
 
 <img src="./GuessingGame-Diagram.png" alt="./GuessingGame-Diagram.png">
 
 #### Assumptions
+
+- User is the person running the code and playing the game
+- GuessingGame is the running program that responds to user input
 - Handling and checking of the user input can be done in any manner
+  - Includes handling of maximum number and maximum guesses
+  - Includes handling of response to "play again?" prompts
+  - Defaults to a maximum number and maximum guesses of 1
+- The messages and logic flows of the PlantUML diagram are implemented as-is and correctly
 
 # Implementation Code
+
 The program was written in VSCode on WSL 2, Ubuntu 20.04.4 LTS.
 
-Refer to `./Asterisk.java` for program. See the code block below (if this is the PDF submission).
+Refer to `./GuessingGame.java` for program. See the code block below (if this is the PDF submission).
 
 ```java
 // code block goes here for the actual PDF submission
 ```
 
 # Implementation Output
+
 Below is an output in the VSCode Integrated Terminal.
 
 <img src="./GuessingGame-Completed.png" alt="./GuessingGame-Completed.png">
 
 # PlantUML
+
 ```
 @startuml GuessingGame
 
