1cb4de83c8b6d5c61013f086cfe80b38cc4a00e2
justinthelaw <justinwingchunglaw@gmail.com>
2022-07-18 21:40:32 -0700

    java project module-08 started

---
+++ programming-in-java/module-08/Manipulater/src/App.java
@@ -0,0 +1,14 @@
+public class App {
+
+    /**
+     * Executes the test code to instantiate 2 Animals and 2 Vehicles in an array,
+     * along with their innate attributes and methods, before calling their
+     * implemented interface methods polymorphically
+     *
+     * @param args
+     * @throws Exception
+     */
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
